782680be6ff3090c54ae04ab7fb9b10d4682daff
python90-p <82655066+python90-p@users.noreply.github.com>
2022-08-22 22:03:18 +0530

    Update 20th August.py
    The above file is the python file which contains the code 👨‍💻

--- 20th August.py
+++ 20th August.py
@@ -1,38 +1,48 @@
+#We first import the fraction module to write fractions . The code inside this module is :
+
+#the code has a variable set for numerator and denominator and divides them and writes in a new line with a dash in middle 
+# this is the code inside the module set in a class.
+
 from fractions import Fraction
 
-en = int(input('Enter Numerator of first fraction: '))
-sn = int(input('Enter denominator of first fraction: '))
+#This while loop means that the interface asks for numerator and denominator until the user exits it.
+i = 1
+while i<=5:
+#The below code asks the input and enters all the basic functions answers.
 
-ff = Fraction(en, sn)
+    en = int(input('Enter Numerator of first fraction: '))
+    sn = int(input('Enter denominator of first fraction: '))
 
-print('  ')
+    ff = Fraction(en, sn)
 
-rn = int(input('Enter Numerator of second fraction: '))
-qn = int(input('Enter denominator of second fraction: '))
+    print('  ')
 
-sf = Fraction(rn, qn)
+    rn = int(input('Enter Numerator of second fraction: '))
+    qn = int(input('Enter denominator of second fraction: '))
 
-print('  ')
+    sf = Fraction(rn, qn)
 
-sumf = ff+sf
-print('Sum of the two fractions is: ')
-print(sumf)
+    print('  ')
 
-print('  ')
+    sumf = ff+sf
+    print('Sum of the two fractions is: ')
+    print(sumf)
 
-subf = ff-sf
-print('Difference of the two fractions is:')
-print(subf)
+    print('  ')
 
-print('  ')
+    subf = ff-sf
+    print('Difference of the two fractions is:')
+    print(subf)
 
-mulf = ff*sf
-print('The product of the two fractions is:')
-print(mulf)
+    print('  ')
 
-print('  ')
+    mulf = ff*sf
+    print('The product of the two fractions is:')
+    print(mulf)
 
-divf = ff/sf
-print('The quotient of the two fractions is:')
-print(divf)
+    print('  ')
+
+    divf = ff/sf
+    print('The quotient of the two fractions is:')
+    print(divf)
 
